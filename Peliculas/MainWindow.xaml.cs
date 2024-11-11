141344bddb973276805b5fb7bc83186c86fe2fa9
Rubensio19 <153018377+FernandoAlonso1433@users.noreply.github.com>
2024-11-11 18:38:09 +0100

    si

---
+++ Peliculas/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Peliculas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string path = "C:/Users/Admin/source/repos/Peliculas/usuarios.txt";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        static async Task Esperar()
+        {
+            await Task.Delay(1000); 
+        }
+        public bool EsCorreoValido(string email)
+        {
+            try
+            {
+                var direccionCorreo = new MailAddress(email);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        private void button_borrar_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox_correo.Clear();
+            PassBox.Clear();
+        }
+        private void AbrirNuevaVentana()
+        {
+            Window1 nuevaVentana = new Window1();
+            
+            nuevaVentana.ShowDialog();
+        }
+        int seguidas = 0;
+        private async void button_confirmar_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> correos = new List<string>();
+            List<string> contraseñas = new List<string>();
+            string[] lineas = File.ReadAllLines(path);
+
+            foreach (string linea in lineas)
+            {
+                if (linea.Contains('|'))
+                {
+                    string[] separa = linea.Split('|');
+                    correos.Add(separa[0]);
+                    contraseñas.Add(separa[1]);
+                }
+            }
+            if (seguidas != 3)
+            {
+                if (EsCorreoValido(txtBox_correo.Text) && PassBox.Password.Length >= 3)
+                {
+
+                    if (correos.Contains(txtBox_correo.Text))
+                    {
+                        int pos = 0;
+                        for (int i = 0; i < correos.Count; i++)
+                        {
+                            if (correos[i] == txtBox_correo.Text)
+                            {
+                                pos = i; break;
+                            }
+                        }
+                        if (contraseñas[pos].Equals(PassBox.Password))
+                        {
+                            MessageBox.Show("Contraseña correcta, bienvenido de nuevo.", "Datos inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Information);
+                            seguidas = 0;
+                            AbrirNuevaVentana();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Contraseña incorrecta", "Error de inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Warning);
+                            seguidas++;
+                            if (seguidas == 3)
+                            {
+                                this.Close();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        using (StreamWriter writer = new StreamWriter(path, append: true))
+                        {
+                            writer.WriteLine(txtBox_correo.Text + "|" + PassBox.Password);
+                        }
+                        MessageBox.Show("Los datos introducidos son válidos para un registro, bienvenido.", "Datos Registro", MessageBoxButton.OK, MessageBoxImage.Information);
+                        seguidas = 0;
+                        AbrirNuevaVentana();
+                    }
+
+                }
+                else
+                {
+                    seguidas++;
+                    MessageBox.Show("La contraseña debe ser mayor a 3 caracteres y el correo debe ser válido.", "Error Registro/Inicio sesión", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    if (seguidas == 3) { 
+                        this.Close();
+                    }
+
+                }
+            }
+            
+        }
+    }
+}
---
+++ Peliculas/Objetos/Pelicula.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peliculas.Objetos
+{
+    public class Pelicula
+    {
+        public String Titulo { get; set; }
+        public int Sala { get; set; }
+        public String Idioma { get; set; }
+        public DateTime diaInici { get; set; }
+        public DateTime diaFinal { get; set; }
+        public TimeSpan Hora { get; set; }
+        public int Duracion { get; set; }
+        public List<String> Generos { get; set; }
+        public Pelicula(String input1, int input2, String input3, DateTime input4, DateTime input5, TimeSpan input6, int input7, List<String> input8)
+        {
+            Titulo = input1;
+            Sala = input2;
+            Idioma = input3;
+            diaInici = input4;
+            diaFinal = input5;
+            Hora = input6;
+            Duracion = input7;
+            Generos = input8;
+        }
+    }
+}
