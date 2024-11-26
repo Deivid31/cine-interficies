67ba56cb3ffd954c4e747a7218b5e85603308b83
David31 <153017116+Deivid31@users.noreply.github.com>
2024-11-26 02:56:57 +0100

    asdasd

--- Peliculas/MainWindow.xaml.cs
+++ Peliculas/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Media;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,46 +19,31 @@ using System.Windows.Shapes;
 
 namespace Peliculas
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         DBMachine db = new DBMachine();
         int seguidas = 0;
         public User userBinding;
+        int numErrors = 0;
 
         public MainWindow()
         {
             InitializeComponent();
+            RefreshUsrAndContent();
         }
-        public bool EsCorreoValido(string email)
-        {
-            try
-            {
-                if (!string.IsNullOrEmpty(email))
-                {
-                    var direccionCorreo = new MailAddress(email);
-                    return true;
-                }
-                return false;
-            }
-            catch (FormatException)
-            {
-                return false;
-            }
-        }
+        
 
         private void RefreshUsrAndContent() {
             userBinding = new User();
-            txtBox_correo.Text = userBinding.mail;
-            txtBox_contra.Text = userBinding.passwd;
+            txtBox_correo.DataContext = userBinding;
+            txtBox_contra.DataContext = userBinding;
         }
 
         private void button_borrar_Click(object sender, RoutedEventArgs e)
         {
             txtBox_correo.Clear();
             txtBox_contra.Clear();
+            RefreshUsrAndContent();
         }
         private void AbrirNuevaVentana(Boolean admin)
         {
@@ -66,59 +52,68 @@ namespace Peliculas
             MainWindow1.Hide();
             nuevaVentana.ShowDialog();
         }
-        
+
+        public void FunctionValidationError(object sender, ValidationErrorEventArgs e)
+        {
+            if (e.Action == ValidationErrorEventAction.Added)
+            {
+                numErrors++;
+            }
+            else
+            {
+                numErrors--;
+            }
+
+            if (numErrors == 0)
+            {
+                button_confirmar.IsEnabled = true;
+            }
+            else
+            {
+                button_confirmar.IsEnabled = false;
+            }
+        }
+
+
         private async void button_confirmar_Click(object sender, RoutedEventArgs e)
         {
             if (seguidas != 3)
             {
-                if (EsCorreoValido(txtBox_correo.Text) && txtBox_contra.Text.Length >= 3)
+                if (db.userExist(userBinding))
                 {
-                    userBinding = new User(txtBox_correo.Text, PassBox.Password);
-                    if (db.userExist(userBinding))
+                    if (db.checkUser(userBinding))
                     {
-                        if (db.checkUser(userBinding))
+                        seguidas = 0;
+                        if (db.isAdmin(userBinding))
                         {
-                            MessageBox.Show("Contraseña correcta, bienvenido de nuevo.", "Datos inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Information);
-                            seguidas = 0;
-                            if (db.isAdmin(userBinding))
-                            {
-                                AbrirNuevaVentana(true);
-                            }
-                            AbrirNuevaVentana(false);
+                            AbrirNuevaVentana(true);
                         }
-                        else
-                        {
-                            MessageBox.Show("Contraseña incorrecta", "Error de inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Warning);
-                            seguidas++;
-                            if (seguidas == 3)
-                            {
-                                this.Close();
-                            }
-                        }
+                        AbrirNuevaVentana(false);
                     }
                     else
                     {
-                        
-                        db.addUser(userBinding);
-                        MessageBox.Show("Los datos introducidos son válidos para un registro, bienvenido.", "Datos Registro", MessageBoxButton.OK, MessageBoxImage.Information);
-                        seguidas = 0;
-                        AbrirNuevaVentana(false);
+                        MessageBox.Show("Contraseña incorrecta", "Error de inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        seguidas++;
+                        if (seguidas == 3)
+                        {
+                            this.Close();
+                        }
                     }
-
                 }
                 else
                 {
-                    seguidas++;
-                    txtBox_correo.Clear();
-                    txtBox_contra.Clear();
-                    MessageBox.Show("La contraseña debe ser mayor a 3 caracteres y el correo debe ser válido.", "Error Registro/Inicio sesión", MessageBoxButton.OK, MessageBoxImage.Warning);
-                    if (seguidas == 3) { 
-                        this.Close();
-                    }
-
+                        
+                    db.addUser(userBinding);
+                    MessageBox.Show("Los datos introducidos son válidos para un registro, bienvenido.", "Datos Registro", MessageBoxButton.OK, MessageBoxImage.Information);
+                    seguidas = 0;
+                    AbrirNuevaVentana(false);
                 }
             }
-            
+        }
+
+        private void controlar_aste(object sender, TextChangedEventArgs e)
+        {
+            txtBox_contra_aste.Text = new string('*', txtBox_contra.Text.Length);
         }
     }
 }
--- Peliculas/Objetos/User.cs
+++ Peliculas/Objetos/User.cs
@@ -2,22 +2,65 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Peliculas.Objetos
 {
-    public class User
+    public class User : IDataErrorInfo
     {
         public String mail { get; set; }
         public String passwd { get; set; }
         public bool admin { get; set; }
 
-
-        public User()
+        public string Error
         {
+            get
+            {
+                return "";
+            }
         }
 
+        public string this[string columnName]
+        {
+            get
+            {
+                string result = "";
+                if (columnName == "mail")
+                {
+                    if (string.IsNullOrEmpty(mail))
+                    {
+                        result = "El email no puede ser nulo";
+                    } else
+                    {
+                        if (!EsCorreoValido(mail))
+                        {
+                            result = "El correo no es válido";
+                        }
+                    }
+                }
+
+                if (columnName == "passwd")
+                {
+                    if (string.IsNullOrEmpty(passwd))
+                    {
+                        result = "La contraseña no puede ser nula";
+                    }
+                    else
+                    {
+                        if (passwd.Length < 3)
+                        {
+                            result = "La contraseña debe tener al menos 3 carácteres";
+                        }
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        public User() {}
 
         public User(String input1, String input2) {
             mail = input1;
@@ -32,5 +75,27 @@ namespace Peliculas.Objetos
         }
 
 
+
+
+        private bool EsCorreoValido(string email)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(email))
+                {
+                    if (email.Contains("."))
+                    {
+                        var direccionCorreo = new MailAddress(email);
+                        return true;
+                    }
+                    return false;
+                }
+                return false;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
     }
 }
