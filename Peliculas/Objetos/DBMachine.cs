f61e7879b810b8713cb11f4315f4bd3f89dbf550
Rubensio19 <153018377+FernandoAlonso1433@users.noreply.github.com>
2024-11-26 15:53:44 +0100

    xd

--- Peliculas/Objetos/DBMachine.cs
+++ Peliculas/Objetos/DBMachine.cs
@@ -165,5 +165,50 @@ namespace Peliculas.Objetos
 
             return peliculasList;
         }
+        public List<Pelicula> insert_Films()
+        {
+            List<Pelicula> films = new List<Pelicula>();
+            MySqlCommand cmd = ConnectDB();
+            cmd.CommandText = @"
+        SELECT f.film_id, f.title, f.room, l.name AS language, f.start_date, f.end_date, f.hour, f.duration
+        FROM film f
+        JOIN language l ON f.language_id = l.language_id
+        JOIN `film-genre` fg ON f.film_id = fg.film_id";
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            List<Pelicula> peliculasList = new List<Pelicula>();
+
+            while (reader.Read())
+            {
+                int filmId = reader.GetInt32("film_id");
+                cmd.CommandText = @"SELECT name FROM genre JOIN `film-genre` ON `film-genre`.genre_id = genre.genre_id JOIN film ON film.film_id = `film-genre`.film_id WHERE film.film_id = " + filmId + ";";
+                MySqlDataReader reader2 = cmd.ExecuteReader();
+                List<String> generos = new List<String>();
+                while (reader2.Read())
+                {
+                    generos.Add(reader2.GetString("name"));
+                }
+                Pelicula pelicula = new Pelicula
+                (
+                    reader.GetString("title"),
+                    reader.GetInt32("room"),
+                    reader.GetString("language"),
+                    reader.GetDateTime("start_date"),
+                    reader.GetDateTime("end_date"),
+                    reader.GetTimeSpan("hour"),
+                    reader.GetInt32("duration"),
+                    generos
+                );
+                peliculasList.Add(pelicula);
+
+
+            }
+
+            reader.Close();
+            cmd.Connection.Close();
+
+            return peliculasList;
+        }
     }
 }
