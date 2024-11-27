c7c2d61a39acb0787fb091c155e2c0196334380e
Rubensio19 <153018377+FernandoAlonso1433@users.noreply.github.com>
2024-11-27 18:25:25 +0100

    Ver pelis

---
+++ Peliculas/cosas/VerPelis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Peliculas.Objetos;
+
+namespace Peliculas.cosas
+{
+    public class VerPelis
+    {
+        public DBMachine dBMachine;
+        public ObservableCollection<Pelicula> listFilms { get; set; }
+
+        public VerPelis()
+        {
+            DBMachine dBMachine = new DBMachine();
+            this.dBMachine = dBMachine;
+            listFilms = new ObservableCollection<Pelicula>(dBMachine.take_Films());
+        }
+        
+    }
+}
